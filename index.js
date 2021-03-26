b9bb7461a475bf8826d56cc6533c3d52984eb0c3
khalid <mdmahfuzbd@gmail.com>
2021-03-27 01:12:55 +0600

    insert one product to mongodb directly done

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 
-app.listen(port,()=>{console.log('I am listening at port 3000')});
+app.listen(port, () => { console.log('I am listening at port 3000') });
 
-app.get('/',(req,res)=>{
-    res.send('hello i am working')
+app.get('/', (req, res) => {
+    // res.send('hello i am working') // sending simple string
+    res.sendFile(__dirname + '/index.html');
 })
+
 
 //mongodb cred
 const password = 'myMongodb123';
@@ -21,10 +23,20 @@ const dbname = 'organicdb';
 //mongodb connection code copied from mongodb web
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://organicUser:myMongodb123@cluster0.lroqv.mongodb.net/organicdb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true ,connectTimeoutMS: 30000,keepAlive: 1});
 client.connect(err => {
-  const collection = client.db("organicdb").collection("products");
-  console.log('db connected 2nd');
-  // perform actions on the collection object
-  client.close();
+    const collection = client.db("organicdb").collection("products");
+    console.log('db connected 2nd');
+
+    //insert data as BSON format Binary form of JSON
+    const product = { name: "modhu", price: 25, qty: 100 };
+    collection.insertOne(product)
+        .then(result => {
+            // process result
+            console.log('One product added');
+        })
+
+    
+    // perform actions on the collection object
+    //client.close();
 });
