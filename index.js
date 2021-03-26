ed8ef9c03eccc4754da69acdfb888c3be552c09a
khalid <mdmahfuzbd@gmail.com>
2021-03-27 02:08:17 +0600

    module 46-5 done

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json()); //no need to use bodyparser
+app.use(express.urlencoded()); //Parse URL-encoded bodies // need this instead of app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, () => { console.log('I am listening at port 3000') });
 
 app.get('/', (req, res) => {
     // res.send('hello i am working') // sending simple string
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html'); //it will show the index.html file under localhost:3000 now
 })
+app.post('/addProduct1/',(req,res)=>{
+    const product = req.body;
+    console.log(product)
 
+});
 
 //mongodb cred
 const password = 'myMongodb123';
@@ -28,13 +32,26 @@ client.connect(err => {
     const collection = client.db("organicdb").collection("products");
     console.log('db connected 2nd');
 
-    //insert data as BSON format Binary form of JSON
-    const product = { name: "modhu", price: 25, qty: 100 };
-    collection.insertOne(product)
+    //get data from index.html's form and send to database
+    app.post('/addProduct/',(req,res)=>{
+        const product = req.body;
+        // console.log(product);
+        collection.insertOne(product)
         .then(result => {
             // process result
             console.log('One product added');
+            res.send('data insert successfully');
         })
+
+    });
+
+    //insert data as BSON format Binary form of JSON
+    // const product = { name: "modhu", price: 25, qty: 100 };
+    // collection.insertOne(product)
+    //     .then(result => {
+    //         // process result
+    //         console.log('One product added');
+    //     })
 
     
     // perform actions on the collection object
